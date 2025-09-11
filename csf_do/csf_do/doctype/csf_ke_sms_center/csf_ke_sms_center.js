926b0770cdde54a4552636febd626c0ebe6d605b
Tu Nombre <tu.email@ejemplo.com>
2025-09-10 22:09:58 -0400

    refactor: actualizar nombre de 'CSF KE SMS Center' a 'CSF DO SMS Center' en archivos JS y JSON

--- csf_do/csf_do/doctype/csf_ke_sms_center/csf_ke_sms_center.js
+++ csf_do/csf_do/doctype/csf_ke_sms_center/csf_ke_sms_center.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2023, Navari Limited and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("CSF KE SMS Center", {
+// frappe.ui.form.on("CSF DO SMS Center", {
 // 	refresh(frm) {
 
 // 	},
